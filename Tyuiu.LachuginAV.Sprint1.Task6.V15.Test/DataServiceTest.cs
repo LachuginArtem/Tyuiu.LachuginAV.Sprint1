88534f9f8063d7b109588b7ec202877888e84646
Tyuiu.LachuginAV@outlook.com <lavtemik@mail.ru>
2023-09-28 20:02:12 +0300

    Task6

---
+++ Tyuiu.LachuginAV.Sprint1.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LachuginAV.Sprint1.Task6.V15.Lib;
+
+namespace Tyuiu.LachuginAV.Sprint1.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public static void ValidString()
+        {
+            DataService ds = new DataService();
+        string value = "ewfwe ef wer wccew";
+        bool res = ds.CheckLettersCount(value);
+        bool wait = true;
+        Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.LachuginAV.Sprint1.Task6.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LachuginAV.Sprint1.Task6.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LachuginAV.Sprint1.Task6.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a084331-51f6-4349-810a-25d9a5386c09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
